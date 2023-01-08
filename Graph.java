0a8fd703bcc2a7abd47fbe8d2c3e3291d416c5d6
James Bachman <hey.james.bachman@gmail.com>
2023-01-08 17:54:35 -0500

    basic circle graph

--- Graph.java
+++ Graph.java
@@ -5,27 +5,27 @@ public class Graph {
     private int maxY;
     private int width;
     private int height;
+    private double resolution;
     private String display = "";
-    private int resolution;
 
-    public Graph(int minX, int maxX, int minY, int maxY, int resolution){
+    public Graph(int minX, int maxX, int minY, int maxY, double resolution){
         this.minX = minX;
         this.maxX = maxX;
         this.minY = minY;
         this.maxY = maxY;
-        width  = this.maxX - this.minX;
-        height = this.maxY - this.minY;
         this.resolution = resolution;
+        width  = (int) ((this.maxX - this.minX) / this.resolution) + 1;
+        height = (int) ((this.maxY - this.minY) / this.resolution) + 1;
     }
 
     public String createDisplay() {
         for(int a = 0; a < 2; a ++){
 
-            for(int i = 0; i < ((height * resolution) / 2); i ++) {
+            for(int i = 0; i < (height / 2); i ++) {
 
                 for(int b = 0; b < 2; b ++){
 
-                    for(int j = 0; j < ((width * resolution) / 2); j ++) {
+                    for(int j = 0; j < (width / 2); j ++) {
                         display += ". ";
                     }
 
@@ -40,7 +40,7 @@ public class Graph {
             if(a < 1) {
                 for(int b = 0; b < 2; b ++){
 
-                    for(int j = 0; j < ((width * resolution) / 2); j ++) {
+                    for(int j = 0; j < (width / 2); j ++) {
                         display += "- ";
                     }
 
@@ -54,6 +54,9 @@ public class Graph {
         return display;
     }
 
+    public String getDisplay() {
+        return display;
+    }
     public int getMinX() {
         return minX;
     }
@@ -79,10 +82,10 @@ public class Graph {
     }
 
     public String graphPoint(Point point1) {
-        double x = point1.getX();
-        double y = point1.getY();
-        
-        
-        return "hello world";
+        double x = point1.getX() / resolution;
+        double y = point1.getY() / resolution;
+        int index = (int) ((((height / 2) - y) * ((width * 2) + 1)) + ((width - 1) + (x * 2)));
+        display = display.substring(0, index) + "#" + display.substring(index + 1, display.length());
+        return display;
     }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
+
 public class Main {
     public static void main(String[] args) {
 
-        Graph graph01 = new Graph(-5, 5, -5, 5, 1);
+        Graph graph01 = new Graph(-10, 10, -10, 10, 1);
+        Point point01 = new Point(0, 0);
+        
+        graph01.createDisplay();
+        // System.out.println(graph01.getDisplay());
+        System.out.println("width is "  + graph01.getWidth());
+        System.out.println("height is " + graph01.getHeight());
+        
+        graph01.graphPoint(point01);
+        // System.out.println(graph01.getDisplay());
 
-        System.out.println(graph01.createDisplay());
-
-
+        for(double a = 0; a < (Math.PI * 4); a += 0.5) {
+            graph01.graphPoint(new Point(((int) (Math.cos(a) * 10)), ((int) (Math.sin(a) * 10))));
+        }
+        System.out.println(graph01.getDisplay());
     }
 }
