dcf6f5cc9987355e9c30f9f77d8f5588585934e7
James Bachman <hey.james.bachman@gmail.com>
2023-01-08 16:50:22 -0500

    basic graph generation

--- Graph.java
+++ Graph.java
@@ -1,3 +1,88 @@
 public class Graph {
-    
+    private int minX;
+    private int maxX;
+    private int minY;
+    private int maxY;
+    private int width;
+    private int height;
+    private String display = "";
+    private int resolution;
+
+    public Graph(int minX, int maxX, int minY, int maxY, int resolution){
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+        width  = this.maxX - this.minX;
+        height = this.maxY - this.minY;
+        this.resolution = resolution;
+    }
+
+    public String createDisplay() {
+        for(int a = 0; a < 2; a ++){
+
+            for(int i = 0; i < ((height * resolution) / 2); i ++) {
+
+                for(int b = 0; b < 2; b ++){
+
+                    for(int j = 0; j < ((width * resolution) / 2); j ++) {
+                        display += ". ";
+                    }
+
+                    if(b < 1) {
+                        display += "| ";
+                    }
+                }
+
+                display += "\n"; 
+            }
+
+            if(a < 1) {
+                for(int b = 0; b < 2; b ++){
+
+                    for(int j = 0; j < ((width * resolution) / 2); j ++) {
+                        display += "- ";
+                    }
+
+                    if(b < 1) {
+                        display += "+ ";
+                    }
+                }
+            }
+            display += "\n";
+        }
+        return display;
+    }
+
+    public int getMinX() {
+        return minX;
+    }
+
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public int getMinY() {
+        return minY;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String graphPoint(Point point1) {
+        double x = point1.getX();
+        double y = point1.getY();
+        
+        
+        return "hello world";
+    }
 }
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Graph graph01 = new Graph(-5, 5, -5, 5, 1);
+
+        System.out.println(graph01.createDisplay());
+
+
+    }
+}
