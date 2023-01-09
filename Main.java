3a8c47db62508d3b882bd059372a56c4f86fd0d5
James Bachman <hey.james.bachman@gmail.com>
2023-01-08 20:56:14 -0500

    removed recursive in favor of slope

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main {
         //     y = (int) (Math.sin(a) * 10);
         //     graph01.graphPoint(new Point(x, y));
         // }
-        Point point02 = new Point(10, 7);
+        Point point02 = new Point(10, 1);
         graph01.graphPoint(point02);
         line(point01, point02, graph01);
         System.out.println(graph01.getDisplay());
@@ -33,49 +33,41 @@ public class Main {
         if(point1.getX() <= point2.getX()){
             ax = point1.getX();
             bx = point2.getX();
-        }else{
-            ax = point2.getX();
-            bx = point1.getX();
-        }
-
-        if(point1.getY() <= point2.getY()){
             ay = point1.getY();
             by = point2.getY();
         }else{
+            ax = point2.getX();
+            bx = point1.getX();
             ay = point2.getY();
             by = point1.getY();
         }
 
-        int xDisp = (int) ((bx - ax));
-        int yDisp = (int) ((by - ay));
+        double xDisp = (bx - ax);
+        double yDisp = (by - ay);
 
         if(xDisp == 0){
-            for(double i = 0; i <= yDisp; i ++){
+            for(int i = 0; i <= yDisp; i ++){
                 graph1.graphPoint(new Point(ax, (ay + i)));
-                // System.out.println("Add point: (" + ax + ", " + i + ")");
             }
 
         }else if(yDisp == 0){
-            for(double i = 0; i <= xDisp; i ++){
+            for(int i = 0; i <= xDisp; i ++){
                 graph1.graphPoint(new Point((ax + i), ay));
-                // System.out.println("Add point: (" + i + ", " + ay + ")");
-            }
-
-        }else if(xDisp == yDisp){
-            for(double i = 0; i <= xDisp; i ++){
-                graph1.graphPoint(new Point((ax + i), (ay + i)));
-                // System.out.println("Add point: (" + i + ", " + i + ")");
             }
 
         }else{
-            int mx = (int) ((xDisp * 0.5));
-            int my = (int) ((yDisp * 0.5));
-            Point pointM = new Point(mx, my);
-            // System.out.println("From: (" + ax + ", " + ay + ") to; (" + bx + ", " + by + ")");
-            line(point1, pointM, graph1);
-            // System.out.println("From: (" + ax + ", " + ay + ") to; (" + bx + ", " + by + ")");
-            line(pointM, point2, graph1);
-        }
-        System.out.println("From: (" + ax + ", " + ay + ") to (" + bx + ", " + by + ")");
+            if(xDisp >= Math.abs(yDisp)){
+                for(int i = 0; i < xDisp; i ++){
+                    graph1.graphPoint(new Point((ax + i), ay));
+                    ay += yDisp/xDisp;
+                }
+
+            }else{
+                for(int i = 0; i < yDisp; i ++){
+                    graph1.graphPoint(new Point(ax, (ay + i)));
+                    ax += xDisp/yDisp;
+                }
+            }
+        } 
     }
 }
